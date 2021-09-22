401dc15cbaf4c13e56e9966d6069442f5e67ee28
Valentin <valentinspasov9669@gmail.com>
2021-09-22 04:22:20 +0300

    .

--- src/context/index.js
+++
@@ -1,8 +0,0 @@
-import { NetworkContextProvider } from './Network/NetworkContext'
-import { ObserverContextProvider } from './Observer/ObserverContext'
-
-export const Contexts = (props) => (
-    <NetworkContextProvider>
-        <ObserverContextProvider>{props.children}</ObserverContextProvider>
-    </NetworkContextProvider>
-)
